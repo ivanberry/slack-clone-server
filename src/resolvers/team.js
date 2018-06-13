de489b4c1ba8ae31e4448073c70bd0073c14cf12
ivanberry <tabvim.dev@gmail.com>
2018-06-13 10:40:51 +0800

    Add Basic authenticated

--- src/resolvers/team.js
+++ src/resolvers/team.js
@@ -1,19 +1,22 @@
 import { formatErrors } from '../utils';
+import requiresAuth from '../permissions';
 
 export default {
   Mutation: {
-    createTeam: async (parent, args, { models, user }) => {
-      try {
-        await models.Team.create({ ...args, owner: user.id });
-        return {
-          ok: true,
-        };
-      } catch (err) {
-        return {
-          ok: false,
-          errors: formatErrors(err, models),
-        };
-      }
-    },
+    createTeam: requiresAuth.createResolver(
+      async (parent, args, { models, user }) => {
+        try {
+          await models.Team.create({ ...args, owner: user.id });
+          return {
+            ok: true,
+          };
+        } catch (err) {
+          return {
+            ok: false,
+            errors: formatErrors(err, models),
+          };
+        }
+      },
+    ),
   },
 };
