bc31d321f727cf715ad99489035d3306a6081b31
ivanberry <tabvim.dev@gmail.com>
2018-06-23 10:26:57 +0800

    .feat create new channel

--- src/resolvers/channel.js
+++ src/resolvers/channel.js
@@ -1,11 +1,19 @@
+import { formatErrors } from '../utils';
+
 export default {
   Mutation: {
     createChannel: async (parent, args, { models }) => {
       try {
-        await models.Channel.create(args);
-        return true;
+        const channel = await models.Channel.create(args);
+        return {
+          ok: true,
+          channel,
+        };
       } catch (err) {
-        return false;
+        return {
+          ok: false,
+          errors: formatErrors(err),
+        };
       }
     },
   },
