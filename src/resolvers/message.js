85a773e6b2e5c74ee36141826eea40a2136dc050
ivanberry <tabvim.dev@gmail.com>
2018-06-28 06:30:17 +0800

    .feat set up subscription socket

--- src/resolvers/message.js
+++ src/resolvers/message.js
@@ -1,25 +1,75 @@
+import { PubSub, withFilter } from 'graphql-subscriptions';
+
+import requiresAuth from '../permissions';
+
+const pubsub = new PubSub();
+
+const NEW_CHANNEL_MESSAGE = 'NEW_CHANNEL_MESSAGE';
+
 export default {
-  Mutation: {
-    createMessage: async (parent, args, { models, user }) => {
-      try {
-        await models.Message.create({ ...args, userId: user.id });
-        return true;
-      } catch (err) {
-        console.log(err);
-        return false;
+  Subscription: {
+    newChannelMessage: {
+      subscribe: withFilter(
+        () => pubsub.asyncIterator(NEW_CHANNEL_MESSAGE),
+        (payload, args) => payload.channelId === args.channelId,
+      ),
+    },
+  },
+  Message: {
+    user: ({ user, userId }, args, { models }) => {
+      if (user) {
+        return user;
       }
+
+      return models.User.findOne({ where: { id: userId } }, { raw: true });
     },
   },
   Query: {
-    messages: async (parent, { channelId }, { models }) =>
-      models.Message.findAll(
-        { order: [['created_at', 'ASC']] },
-        { where: { channelId } },
-        { raw: true },
-      ),
+    messages: requiresAuth.createResolver(
+      async (parent, { channelId }, { models }) =>
+        models.Message.findAll(
+          { order: [['created_at', 'ASC']], where: { channelId } },
+          { raw: true },
+        ),
+    ),
   },
-  Message: {
-    user: ({ userId }, args, { models }) =>
-      models.User.findOne({ where: { id: userId } }, { raw: true }),
+  Mutation: {
+    createMessage: requiresAuth.createResolver(
+      async (parent, args, { models, user }) => {
+        try {
+          const message = await models.Message.create({
+            ...args,
+            userId: user.id,
+          });
+
+          const asyncFunc = async () => {
+            const currentUser = await models.User.findOne({
+              where: {
+                id: user.id,
+              },
+            });
+
+            const newChannelMessage = {
+              ...message.dataValues,
+              user: currentUser.dataValues,
+            };
+
+            console.log(newChannelMessage);
+
+            pubsub.publish(NEW_CHANNEL_MESSAGE, {
+              channelId: args.channelId,
+              newChannelMessage,
+            });
+          };
+
+          asyncFunc();
+
+          return true;
+        } catch (err) {
+          console.log(err);
+          return false;
+        }
+      },
+    ),
   },
 };
